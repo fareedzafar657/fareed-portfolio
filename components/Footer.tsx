6de56208f394e5b0adf06759d9e0ff20827d0e44
Fareed <fareed.zafar@zimogroup.org>
2024-11-11 17:35:45 +0500

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
+import { socialMedia } from "@/data";
+
+const Footer = () => {
+  return (
+    <footer className="w-full mb-[50px] md:mb-5 pb-10" id="contact">
+      <div className="w-full absolute left-0 -bottom-72 min-h-96 overflow-hidden">
+        <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50"
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to take <span className="text-purple">your</span> digital
+          presence to the next level?
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me today and let&apos;s discuss how i can help you
+          achieve your goals.
+        </p>
+        <a href="mailto:fareedzafar657@gmail.com">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center space-y-10 lg:space-y-0">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Fareed Zafar
+        </p>
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile) => (
+            <a
+              key={profile.id}
+              href={profile.link}
+              target="_blank"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-150 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={profile.img} alt={profile.id} width={20} height={20} />
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://91c17d9e31e5bde40b66f72dc2fe6d14@o4508144206020608.ingest.us.sentry.io/4508144218931200",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
